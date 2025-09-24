d7533465afd55595bd1b7f8ffa542d0458866307
GOURAV JENA <gouav.jena.cse.2023@nist.edu>
2025-09-24 07:58:01 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+#Q1--
+# Perform Basic Mathematical Operations : 
+
+a=int(input("Enter the 1st number : "))
+b=int(input("Enter the 2nd number : "))
+print("Addition : ",a+b,"\nSubstraction : ",a-b,"\nMultiplication : ",a*b,"\nDivision : ",a/b)
+
+# Q2--
+# Create a Personalized Greeting
+
+fn=input("Enter your First Name : ") 
+ln=input("Enter your Last Name : ")
+print(f"Hello, {fn} {ln}! welcome to the Python program.")
